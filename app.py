9da09cef122332fc074cc521fbd722f6d8c700ce
s215098 <s215098@dtu.dk>
2025-11-16 23:03:20 +0100

    testing

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
-import plotly.express as px
-import plotly.graph_objects as go
+# import plotly.express as px
+# import plotly.graph_objects as go
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
